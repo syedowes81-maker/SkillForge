422e7c460fed3e9e7272d46ec3f2eb8e0c929018
Syed Owes <owesjaleel01@gmail.com>
2026-07-04 21:54:39 +0530

    Create super user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+class CustomUserAdmin(UserAdmin):
+ list_display=(
+  "username",
+  "email",
+  "phone_number",
+  "can_freelance",
+  "can_hire",
+  "is_staff"
+)
+admin.site.register(User,CustomUserAdmin)
+
--- accounts/models.py
+++ accounts/models.py
@@ -2,3 +2,5 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
  phone_number = models.CharField(max_length=15,blank=True)
+ can_freelance=models.BooleanField(default=False)
+ can_hire=models.BooleanField(default=False)
