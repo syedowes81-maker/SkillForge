42a3cfc2512aeaa81a13e00af2921e323660d74b
Syed Owes <owesjaleel01@gmail.com>
2026-07-05 21:32:47 +0530

    Create OneToOne Field

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,15 @@ class User(AbstractUser):
  phone_number = models.CharField(max_length=15,blank=True)
  can_freelance=models.BooleanField(default=False)
  can_hire=models.BooleanField(default=False)
+
+class FreelancerProfile(models.Model):
+ user=models.OneToOneField(
+  User,
+  on_delete=models.CASCADE
+)
+
+ skills=models.CharField(max_length=255,default=0)
+ bio=models.TextField(default=0)
+ experience=models.IntegerField(default=0)
+ hourly_rate=models.IntegerField(default=0)
+ portfolio=models.URLField(blank=True)
