bd2fcb35a740be5955576554f9f5a6c23492a71b
Syed Owes <owesjaleel01@gmail.com>
2026-07-08 20:20:32 +0530

    Add RegistrationForm and registration URL

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .forms import RegistrationForm 
+
+def register(request):
+  form=RegistrationForm()
+  return render(request,"register.html",{"form":form})
 
 # Create your views here.
