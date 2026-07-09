4be06ce4a546ead8a42fdbdecb1916ad845e2f8b
Syed Owes <owesjaleel01@gmail.com>
2026-07-09 20:34:49 +0530

    complete user registration feature

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,19 +1,24 @@
 from django import forms
-from .models import user
+from .models import User
 
 class RegistrationForm(forms.ModelForm):
-  password=forms.CharField(widget=forms.PasswordInput)
-  confirm_password=forms.CharField(widget=forms.PasswordInput)
-  class Meta:
-    model=user
-    field=[
-      "username",
-      "email",
-    ]
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "email",
+        ]
+
     def clean(self):
-     cleaned_data=super().clean()
-     password=cleaned_data.get("password")
-     confirm_password=cleaned_data.get("confirm_password")
-     if password !=confirm_password:
-       raise forms.validateError("Passwords do not match.")
-       return cleaned_data
+        cleaned_data = super().clean()
+
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError("Passwords do not match.")
+
+        return cleaned_data
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
-from .forms import RegistrationForm 
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
 
 def register(request):
-  form=RegistrationForm()
-  return render(request,"register.html",{"form":form})
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
 
-# Create your views here.
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data["password"])
+            user.save()
+
+            return redirect("/register/")
+
+    else:
+        form = RegistrationForm()
+
+    return render(request, "register.html", {"form": form})
