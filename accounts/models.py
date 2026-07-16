33955b90cc2d3a300c4e14fe184c180cfa668895
Syed Owes <owesjaleel01@gmail.com>
2026-07-16 22:13:33 +0530

    enhance feelancer profile model

--- accounts/models.py
+++ accounts/models.py
@@ -11,10 +11,12 @@ class FreelancerProfile(models.Model):
   on_delete=models.CASCADE
 )
 
- skills=models.CharField(max_length=255,default=0)
- bio=models.TextField(default=0)
- experience=models.IntegerField(default="")
- hourly_rate=models.IntegerField(default="")
+ skills=models.CharField(max_length=255,blank=True)
+ bio=models.TextField(blank=True)
+ experience=models.PositiveIntegerField(default=0)
+ hourly_rate=models.DecimalField(max_digits=8,decimal_places=2,default=0)
+ location=models.CharField(max_length=100,blank=True)
+ profile_picture=models.ImageField(upload_to="profiles/",blank=True,null=True)
  portfolio=models.URLField(blank=True)
 
  def __str__(self):
