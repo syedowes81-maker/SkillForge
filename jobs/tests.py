35f268245c5ded9ce4845d6903d40da88720404f
syedowes81-maker <syedowesjaleel81@gmail.com>
2026-08-22 21:15:06 +0530

    test:add job model creation test

--- jobs/tests.py
+++ jobs/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from .models import Job
+
+
+User = get_user_model()
+
+
+class JobModelTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testclient",
+            password="testpass123"
+        )
+
+    def test_job_creation(self):
+        job = Job.objects.create(
+            client=self.user,
+            category="Python",
+            title="Python Developer",
+            description="Need a Python developer",
+            budget=10000,
+            location="Bangalore"
+        )
+
+        self.assertEqual(job.title, "Python Developer")
+        self.assertEqual(job.category, "Python")
+        self.assertEqual(job.budget, 10000)
+        self.assertEqual(job.location, "Bangalore")
+        self.assertEqual(job.client, self.user)
